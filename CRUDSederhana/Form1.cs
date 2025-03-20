54ff196e3aa6ff104ce4fbccbdf0716f5e3e65b7
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-03-21 03:06:32 +0800

    Inisialisasi komponen form         }

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -7,5 +7,13 @@ namespace CRUDSederhana
 {
     public partial class Form1 : Form
     {
-        // String koneksi ke database SQL Server
+       
         private string connectionString = "Data Source=LAPTOP-M2UCAR4E\\SHIDQUL;Initial Catalog=OrganisasiMahasisma;Integrated Security=True";
+
+
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+       
